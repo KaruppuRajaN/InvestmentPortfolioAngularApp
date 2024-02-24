7b10e8a367b689f45ff8afd19c68d424a49ddb6f
KaruppuRajaN <karuppuraja21@gmail.com>
2024-02-24 12:04:21 +0000

    Stage 1 - MutualFunds Completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,49 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LoginComponent } from './login/login/login.component';
+import { DisplayoptionsComponent } from './mutualfunds/displayoptions/displayoptions.component';
+import { BuymutualfundsComponent } from './mutualfunds/buymutualfunds/buymutualfunds.component';
+import { OwnmutualfundsComponent } from './mutualfunds/ownmutualfunds/ownmutualfunds.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { RoundPipe } from './custom.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DisplayoptionsComponent,
+    BuymutualfundsComponent,
+    OwnmutualfundsComponent,
+    RoundPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    FormsModule
   ],
   providers: [
     provideClientHydration(),
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
---
+++ src/app/mutualfunds/buymutualfunds/buymutualfunds.component.ts
@@ -0,0 +1,108 @@
+import { Component } from '@angular/core';
+import { InvestmentappService } from '../../service/investmentapp.service';
+import { MutualFunds } from '../../model/mutualfunds';
+import {PurchasedMutualFunds} from '../../model/purchasedmutualfunds';
+
+@Component({
+  selector: 'app-buymutualfunds',
+  templateUrl: './buymutualfunds.component.html',
+  styleUrl: './buymutualfunds.component.css'
+})
+export class BuymutualfundsComponent {
+  capsCategory:string='';
+  timestampvalue:string;
+  pmfcount=0;
+  buttonClick:boolean=false;
+  actionDate:string='';
+  riskCategory:string='';
+  paymentAmount:number|null=null;
+  mutualfunds:MutualFunds[][]=[];
+  mutualStockValue:number[]=[];
+  expectedReturnValue:number[]=[];
+  count:number=0;
+  pmfdata:MutualFunds[]=[];
+  purchasedMF:PurchasedMutualFunds[]=[];
+  constructor(private service:InvestmentappService){}
+
+  ngOnInit():void{
+  }
+  getAllMutualFunds(capsCategory:string,riskCategory:string,paymentAmount:number){
+    this.service.getAllMutualFunds({capsCategory,riskCategory,paymentAmount}).subscribe(
+      (response)=>{
+        this.count=0;
+        this.expectedReturnValue=[];
+        this.mutualStockValue=[];
+        this.mutualfunds=response;
+        
+        this.mutualfunds.forEach(mf=>{
+          mf.forEach(mutualF=>{
+            if(mutualF.lastActionDate!=undefined)
+                this.actionDate=mutualF.lastActionDate;
+            if(isNaN(this.mutualStockValue[this.count]))
+                this.mutualStockValue[this.count]=0;
+          if(isNaN(this.expectedReturnValue[this.count]))
+            this.expectedReturnValue[this.count]=0;
+            if(mutualF.calculatedAnnualAmount!=undefined)
+              this.mutualStockValue[this.count]+=mutualF.calculatedAnnualAmount;
+            if(mutualF.calculatedAnnualReturnAmount!=undefined)
+              this.expectedReturnValue[this.count]+=mutualF.calculatedAnnualReturnAmount;
+          });
+          this.count+=1;
+        });
+      },
+      (error)=>{
+      });
+  }
+  onSubmit(){
+    
+    if(this.capsCategory==''||this.capsCategory==undefined)
+      return;
+    this.getAllMutualFunds(this.capsCategory,this.riskCategory,this.paymentAmount);
+    this.buttonClick=true;
+  }
+  changeOptions(id:number){
+    if(id==2){
+      if(this.capsCategory=='Large')
+        this.riskCategory='Low';
+      else if(this.capsCategory=='Mid')
+        this.riskCategory='Medium';
+      else if(this.capsCategory=='Small')
+        this.riskCategory='High';
+    }
+    if(id==1){
+      if(this.riskCategory=='High')
+        this.capsCategory='Small';
+      else if(this.riskCategory=='Medium')
+        this.capsCategory='Mid';
+      else if(this.riskCategory=='Low')
+        this.capsCategory='Large';
+    }
+  }
+
+  purchaseMF(mf:MutualFunds[]){
+    this.pmfdata=mf;
+    this.pmfcount=0;
+    this.timestampvalue=new Date(Date.now()).toString();
+    this.pmfdata.forEach(pm=>{
+      this.purchasedMF[this.pmfcount]=new PurchasedMutualFunds();
+      this.purchasedMF[this.pmfcount].userId=1; //Temporary
+      this.purchasedMF[this.pmfcount].purchaseId=this.timestampvalue;
+      this.purchasedMF[this.pmfcount].boughtAmount=pm.calculatedAnnualAmount;
+      this.purchasedMF[this.pmfcount].boughtPercentage=pm.allocationPercentageFromMoney;
+      this.purchasedMF[this.pmfcount].companyName=pm.companyName;
+      this.purchasedMF[this.pmfcount].companySymbol=pm.companySymbol;
+      this.purchasedMF[this.pmfcount].currentReturnAmount=pm.calculatedAnnualReturnAmount;
+      this.pmfcount+=1;
+      
+    });
+    this.service.purchaseMutualFunds(this.purchasedMF).subscribe(
+      (response)=>{
+        alert("Purchasing is "+response.toString()+" !!!");
+      },
+      (error)=>{
+        alert("Purchasing is "+error.toString()+" !!!");
+      }
+    );
+  }
+
+}
